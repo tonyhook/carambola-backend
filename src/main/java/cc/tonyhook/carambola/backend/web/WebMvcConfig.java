37f9ae6fb683af016286ecacfdd4b950a427fa4a
Tony Hook <tonyhook.su@gmail.com>
2025-03-27 22:53:29 +0800

    [base] use Angular as frontend
    build frontend project while packaging backend project
    deploy files at resources/static
    CORS: allow all request

---
+++ src/main/java/cc/tonyhook/carambola/backend/web/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package cc.tonyhook.carambola.backend.web;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedHeaders("*")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+            .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/static/")
+            .setCachePeriod(3600)
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver() {
+                @Override
+                public Resource getResource(String resourcePath, Resource location) throws IOException {
+                    Resource requestedResource = location.createRelative(resourcePath);
+                    return requestedResource.exists() && requestedResource.isReadable()
+                        ? requestedResource
+                        : new ClassPathResource("/static/index.html");
+                }
+            });
+    }
+
+}
